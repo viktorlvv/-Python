e2f5b334f3109cf6b5bdd5938000525c4f9687d0
Viktor <viktorlvv@mail.ru>
2024-08-04 11:56:20 +0300

    Init

---
+++ Homework 2.4.py
@@ -0,0 +1,24 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []
+not_primes = []
+num = numbers[0]
+for i in range(len(numbers)):
+    is_prime = False
+    num = numbers[i]
+    a = num - 1
+    for j in range(2, num):
+        if num == 1:
+            break
+        if num % j == 0:
+                is_prime = True
+                if j != a:
+                     continue
+    if is_prime:
+        not_primes.append(num)
+
+    else:
+        primes.append(num)
+
+
+print("Простые числа: ", primes)
+print("Составные числа: ", not_primes)
