11485b8ac0f93e3dc1229a534d724fa4dda99256
Viktor <viktorlvv@mail.ru>
2024-08-05 20:54:49 +0300

    Init

---
+++ homework1.py
@@ -0,0 +1,6 @@
+var_DZ = 12
+var_hours = 1.5
+name_Course = 'Python'
+var_Time = var_hours/var_DZ
+print('Курс:',name_Course,'всего задач:',var_DZ,', затрачено часов:',var_hours,', среднее время выполнения:',var_Time,'часа.')
+print(f'Курс: {name_Course}, всего задач:{var_DZ}, затрачено часов:{var_hours}, среднее время выполнения:{var_Time}, часа.')
