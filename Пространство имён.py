c51450121f24dec97ed9e17e554c6a4664f11fa6
Viktor <viktorlvv@mail.ru>
2024-08-25 22:27:50 +0300

    Init

--- Пространство имён.py
+++ Пространство имён.py
@@ -1,13 +1,27 @@
 calls = 0
 def count_calls():  #изменяет значение переменной calls
+    global calls
+    calls = calls + 1
 
 
-def count_calls():  # Считает вызовы остальных функций
+
+def string_info(string: str):  # Принимает аргумент - строку и возвращает кортеж из: длины этой строки, строку в верхнем регистре, строку в нижнем регистре.
+    count_calls()
+    l = len(string)
+    upper = string.upper()
+    lower = string.lower()
+    return l, upper, lower
 
 
-def string_info(string):  # Принимает аргумент - строку и возвращает кортеж из: длины этой строки, строку в верхнем регистре, строку в нижнем регистре.
-    str.split() # разделяет строку на список
-    tuple() # преобразует список в кортеж
+def is_contains(string: str, list_to_search: list):  # принимает два аргумента: строку и список
+    count_calls()
+    for item in list_to_search:
+        if item.lower() == string.lower():
+            return True
+    return False
 
-
-def is_contains(string, list_to_search):  # принимает два аргумента: строку и список
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(calls)
